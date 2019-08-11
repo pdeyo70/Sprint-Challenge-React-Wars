4963a7f4c3d6b8c0eb9bfd64d57dbe08019266c1
Paul DeYonghe <p.deyonghe@gmail.com>
2019-08-10 21:32:34 -0500

    found some styling-related tasks needed for MVP; accomplished these

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import CardCont from './components/CardCont';
 import axios from 'axios';
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-const [data, setData] = useState();
+const [data, setData] = useState([]);
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -15,7 +16,7 @@ useEffect(() => {
     await axios
       .get(`https://swapi.co/api/people/`)
       .then(res => {
-        setData(res.data)
+        setData(res.data.results)
       })
   };
   fetchData()
@@ -29,9 +30,8 @@ console.log(data)
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      <p>{JSON.stringify(data)}</p>
-    </div>
-  );
+      <CardCont data={data} />
+    </div>)
 }
 
 export default App;
